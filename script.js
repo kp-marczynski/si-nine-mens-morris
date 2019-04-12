94948e54409f022e3bf4f27ce012ec9ec944dc05
kp-marczynski <kp.marczynski@gmail.com>
2019-04-12 20:28:26 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,120 @@
+const baseSize = 50;
+const offset = 50;
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let firstPlayer = true;
+
+const circles = [
+    {x: 0, y: 0, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 3, y: 0, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 6, y: 0, radius: 7, color: 'rgba(0,0,0,255)'},
+
+    {x: 1, y: 1, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 3, y: 1, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 5, y: 1, radius: 7, color: 'rgba(0,0,0,255)'},
+
+    {x: 2, y: 2, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 3, y: 2, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 4, y: 2, radius: 7, color: 'rgba(0,0,0,255)'},
+
+    {x: 0, y: 3, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 1, y: 3, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 2, y: 3, radius: 7, color: 'rgba(0,0,0,255)'},
+
+    {x: 4, y: 3, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 5, y: 3, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 6, y: 3, radius: 7, color: 'rgba(0,0,0,255)'},
+
+    {x: 2, y: 4, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 3, y: 4, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 4, y: 4, radius: 7, color: 'rgba(0,0,0,255)'},
+
+    {x: 1, y: 5, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 3, y: 5, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 5, y: 5, radius: 7, color: 'rgba(0,0,0,255)'},
+
+    {x: 0, y: 6, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 3, y: 6, radius: 7, color: 'rgba(0,0,0,255)'},
+    {x: 6, y: 6, radius: 7, color: 'rgba(0,0,0,255)'},
+];
+
+function isIntersect(point, circle) {
+    return Math.sqrt((point.x - getRealCoordinate(circle.x)) ** 2 + (point.y - getRealCoordinate(circle.y)) ** 2) < circle.radius;
+}
+
+function drawSquare(x, y, size, shouldFill) {
+    const finalSize = size * baseSize;
+    const finalX = getRealCoordinate(x);
+    const finalY = getRealCoordinate(y);
+    ctx.beginPath();
+    if (shouldFill) {
+        ctx.fillStyle = 'rgba(255,255,255,255)';
+        ctx.border =
+            ctx.fillRect(finalX, finalY, finalSize, finalSize);
+    } else {
+        ctx.rect(finalX, finalY, finalSize, finalSize);
+    }
+    ctx.stroke();
+}
+
+function getRealCoordinate(val) {
+    return val * baseSize + offset;
+}
+
+function drawBoard() {
+    drawSquare(0, 0, 3);
+    drawSquare(3, 0, 3);
+    drawSquare(0, 3, 3);
+    drawSquare(3, 3, 3);
+    drawSquare(1, 1, 4);
+    drawSquare(2, 2, 2, true);
+    drawSquare(2, 2, 2);
+}
+
+function getMousePos(evt) {
+    let rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+function drawCircle(circle) {
+    ctx.beginPath();
+    const finalX = getRealCoordinate(circle.x);
+    const finalY = getRealCoordinate(circle.y);
+    ctx.arc(finalX, finalY, circle.radius, 0, 2 * Math.PI);
+    ctx.fillStyle = circle.color;
+    ctx.fill();
+}
+
+(function main() {
+    drawBoard();
+
+    circles.forEach(circle => {
+        drawCircle(circle);
+    });
+
+
+    canvas.addEventListener('click', (e) => {
+        const pos = {
+            x: e.clientX,
+            y: e.clientY
+        };
+        const relativePosition = getMousePos(e);
+        circles.forEach(circle => {
+            const pixel = ctx.getImageData(relativePosition.x, relativePosition.y, 1, 1).data;
+            if (isIntersect(relativePosition, circle)) {
+                if (circle.color === 'rgba(' + pixel + ')') {
+                    if (firstPlayer) {
+                        drawCircle({x: circle.x, y: circle.y, radius: circle.radius * 2, color: 'rgba(255,0,0,255)'});
+                    } else {
+                        drawCircle({x: circle.x, y: circle.y, radius: circle.radius * 2, color: 'rgba(0,255,0,255)'});
+                    }
+                    firstPlayer = !firstPlayer;
+                }
+
+            }
+        });
+    });
+})();
